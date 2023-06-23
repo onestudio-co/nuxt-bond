a96a6d4d3e34ffdeca2792ec9771ce9b3dca4660
Mamoun Abu Salah <70561744+Mamoun2020@users.noreply.github.com>
2023-06-23 11:16:55 +0300

    refactor name and mange env nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,13 @@
 export default defineNuxtConfig({
     modules: [
         '@nuxtjs/tailwindcss'
-    ]
+    ],
+    runtimeConfig: {
+        // Private config that is only available on the server
+        apiSecret: process.env.BASE_URL || 'secret',
+        // Config within public will be also exposed to the client
+        public: {
+            baseURL: process.env.API_SECRET || 'https://api.example.com/',
+        },
+    },
 })
