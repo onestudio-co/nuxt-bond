812aaffe7441757a69de68aa16829f5bb2c00ea2
Mohammed S.Abukaresh <52927549+mhmadskarsh98@users.noreply.github.com>
2023-07-07 10:33:45 -0700

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -5,7 +5,10 @@ module.exports = {
       port: '3001',
       exec_mode: 'cluster',
       instances: 'max',
-      script: './.output/server/index.mjs'
+      script: './.output/server/index.mjs',
+      env_production: {
+        NODE_ENV: 'production'
+      }
     }
   ]
 }
