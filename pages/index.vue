ba0f1c262a92bc2206e28b82be2c6d95a16a3b3b
Hala S.A. Abusalim <hala.s.salim@gmail.com>
2023-07-30 23:50:18 +0300

    uses class  from main.scss

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="bond">
-    <h1 class="text-5xl font-bold mb-4">Welcome To</h1>
-    <h2 class="text-5xl font-bold mb-4">Nuxt Bond</h2>
+    <h1 class="text-5xl font-bold mb-4 color-primary">Welcome To</h1>
+    <h2 class="text-5xl font-bold mb-4 color-secondary">Nuxt Bond</h2>
   </div>
 </template>
 
@@ -17,12 +17,5 @@
   justify-content: center;
   align-items: center;
   height: 100vh;
-}
-
-h1 {
-  color: #332D2B;
-}
-h2 {
-  color: #ED6B52;
 }
 </style>
